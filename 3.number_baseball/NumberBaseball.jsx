6c08807ad4913ff9c2de3072ea169086dce97d34
haerim95 <tjffldi123@daum.net>
2021-04-25 23:45:38 +0900

    숫자야구 작업중

--- 3.number_baseball/NumberBaseball.jsx
+++ 3.number_baseball/NumberBaseball.jsx
@@ -2,25 +2,45 @@
 import React, { Component } from 'react';
 import Try from './Try'; //프로젝트가 커지면 길어지기 때문에 컴퍼넌트를 나눠서 관리하면 코드관리가 깔끔해진다.
 
-function getNumbers(){// 쑷자 네 개를 겹치지 않고 랜덤하게 뽑는 함수
 
+function getNumbers(){ //숫자 네개를 겹치지 않고 랜덤하게 뽑는 함수
+    const candidate = [1,2,3,4,5,6,7,8,9];
+    const array = [];
+
+    for(let i =0; i < 4; i += 1){
+        const chosen = candidate.splice(Math.floor(Math.random() * ( 9 -i )), 1)[0];
+        array.push(chosen);
+    }
+    return array;
 }
-
 
 class NumberBaseball extends Component{
     state = {
         result :'',
         value: '',
-        answer: getNumbers(),
-        tries: [],
+        answer: getNumbers(), // ex[1,3,5,7]
+        tries: [], //push 쓰면 안돼!
     };
 
-    onSubmitForm = () => {
-
+    onSubmitForm = (e) => {
+        {/* 화살표 함수를 쓰는 이유 : this를 사용하지 못한다. 그냥 함수불러오려면 constructor 사용해야함 */}
+        if(this.state.value === this.state.answer.join('')){ 
+            this.setState({
+                result : '홈런!',
+                tries: [...this.state.tries, { try: this.state.value, result: '홈런!' }]
+            })
+        }else{
+            const answerArray = this.state.value.split('').map((v)=> parseInt(v));
+            let strike = 0;
+            let ball = 0;
+        }
     };
 
-    onChangeInput = () => {
-
+    onChangeInput = (e) => {
+        console.log(this.state.answer);
+        this.setState({
+            value: e.target.value,
+        })
     };
     
     fruits = [
@@ -39,14 +59,14 @@ class NumberBaseball extends Component{
             </form>
             <div>시도 : {this.state.tries.length}</div>
             <ul>
-                {this.fruits.map((e, i)=>{ //컴퍼넌트엔 인자가 전달되지 않는다
+                {this.fruits.map((v, i)=>{ //컴퍼넌트엔 인자가 전달되지 않는다
                     return(
                              //key: 고유한 값, key를 입력하지 않으면 key 에러가 뜬다. 
                             // key값이 겹치거나 고유하지 않으면 또 에러가 뜬다. 
                             //key에 index 값 입력하는건 지양하자...성능 최적화에 좋지 않음.
-                        <Try key={e.fruit + e.color } value={e} index={i} /> //반복문에는 성능문제가 자주 발생한다
+                        <Try key={v.fruit + v.color } value={v} index={i} /> //반복문에는 성능문제가 자주 발생한다
                         // 인자가 전달되지 않기 때문에 연결고리를 만들어주는데 이게 바로 props이다.
-                        // 이름은 아무거나 써도 된다. chicken={e}로 써도됨 그대신 해당 컴퍼넌트에 정해준 이름으로 불러오기
+                        // 이름은 아무거나 써도 된다. chicken={v}로 써도됨 그대신 해당 컴퍼넌트에 정해준 이름으로 불러오기
                     );
                 })}
             </ul>

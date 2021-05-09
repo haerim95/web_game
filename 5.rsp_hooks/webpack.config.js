bf7506cd4eb8439fb4b87964bec9cce2236f059e
haerim95 <tjffldi123@daum.net>
2021-05-10 01:17:20 +0900

    가위 바위 보 hooks

---
+++ 5.rsp_hooks/webpack.config.js
@@ -0,0 +1,54 @@
+const path = require('path'); //node에소 path를 가져오고
+const RefreshWebpackPlugin = require('@pmmmwh/react-refresh-webpack-plugin'); 
+
+module.exports = {
+    mode: 'development', // 실서비스는 production, 개발 모드는 development
+    devtool : 'inline-source-map', // 속도 빠르게
+    resolve: {
+        extensions: ['.js', '.jsx'], //확장자 찾아서 확장자 붙여준다.
+    },
+
+    // 중요한 두가지
+    entry : { //입력
+        app: ['./client'], //client.jsx에서 WordReplay.jsx를 불러오고 있기때문에 따로 적어주지 않아도 된다. 웹팩이 알아서 다 파악한다
+    },
+
+    module: {
+        rules: [{
+            test: /\.jsx?/,
+            loader: 'babel-loader',
+            options: {
+                //바벨에 대한 설정
+                presets: [
+                    ['@babel/preset-env', {
+                        targets: {
+                            browsers: ['> 1% in KR'], //browserslist
+                        },
+                        debug: true,
+                    }],
+                    '@babel/preset-react'
+                ],
+                //presets만 일단 설치해보고 에러가 뜬다면 에러뜨는걸 추가로 깔아주는 게 좋다
+                plugins: [
+                    '@babel/plugin-proposal-class-properties',
+                    'react-refresh/babel',
+                ],
+            },
+        }],
+    },
+
+    plugins: [
+        new RefreshWebpackPlugin(),
+    ],
+
+    output: { //출력
+        path: path.join(__dirname, 'dist'), //__dirname : 현재폴더, 즉 현재 폴더에 있는 dist
+        ///Users/haerim/haerim/workspace/react/web_game 라는 경로를 일일이 입력해주긴 귀찮으니 path를 사용하는 것이다.
+        filename: 'app.js',
+        publicPath: '/dist/',
+    },
+    devServer: {
+        publicPath: '/dist/',
+        hot: true, 
+    },
+};

a4a04bc6efd85ae20243ed8fa36d9f32c2d27466
haerim95 <tjffldi123@daum.net>
2021-05-21 01:29:00 +0900

    틱택토 완성

--- 7.tictactoe/Td.jsx
+++ 7.tictactoe/Td.jsx
@@ -1,7 +1,18 @@
-import React, { useCallback } from 'react';
+import React, { useCallback, useEffect, useRef, memo } from 'react';
 import { CLICK_CELL } from './TicTacToe';
 
-const Td = ( {rowIndex, cellIndex, dispatch, cellData} ) => {
+const Td = memo(( {rowIndex, cellIndex, dispatch, cellData} ) => {
+  console.log('td rendered');
+
+  //문제 발생했을때 확인하는 법, useRef, useEffect 사용하여 검사
+  const ref = useRef([]);
+  useEffect(() => {
+    console.log(rowIndex === ref.current[0], cellIndex === ref.current[1], dispatch === ref.current[2], cellData === ref.current[3]);
+    console.log(cellData);
+    ref.current = [rowIndex, cellIndex, dispatch, cellData];
+  }, [rowIndex, cellIndex, dispatch, cellData]);
+  // 문제 검사 끝
+
   const onClickTd = useCallback(() => {
     console.log(cellData);
     console.log(rowIndex, cellIndex);
@@ -14,6 +25,6 @@ const Td = ( {rowIndex, cellIndex, dispatch, cellData} ) => {
   return(
     <td onClick={onClickTd}>{ cellData }</td>
   )
-};
+});
 
 export default Td;
--- 7.tictactoe/Tr.jsx
+++ 7.tictactoe/Tr.jsx
@@ -1,15 +1,15 @@
-import React from 'react';
+import React, {memo} from 'react';
 import Td from './Td';
 import Table from './Table';
 
-const Tr = ({ rowData, rowIndex, dispatch }) => {
+const Tr = memo(({ rowData, rowIndex, dispatch }) => {
   return(
     <tr>
       {Array(rowData.length).fill().map((td, i)=>(
-        <Td key={i} dispatch={dispatch} rowIndex={rowIndex} cellIndex={i} cellData={rowIndex[i]} >{''}</Td>
+        <Td key={i} dispatch={dispatch} rowIndex={rowIndex} cellIndex={i} cellData={rowData[i]} >{''}</Td>
       ))}
     </tr>
   )
-};
+});
 
 export default Tr;

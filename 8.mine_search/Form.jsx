9b49854240df7e9d63f1f936d78c755a195adb69
haerim95 <tjffldi123@daum.net>
2021-05-24 23:17:04 +0900

    지뢰찾기 초기 작업

---
+++ 8.mine_search/Form.jsx
@@ -0,0 +1,35 @@
+import React, {useState, useCallback} from 'react';
+
+const Form = () => {
+  const [row, setRow] = useState(10); //세로 몇줄?
+  const [cell, setCell] = useState(10); //가로 몇줄?
+  const [mine, setMine] = useState(20); //지뢰 몇개?
+
+  const onChangeRow = useCallback((e) => {
+    setRow(e.target.value);
+  }, []);
+
+  const onChangeCell = useCallback((e) => {
+    setCell(e.target.value);
+  }, []);
+
+  const onChangeMine = useCallback((e) => {
+    setMine(e.target.value);
+  }, []);
+
+  const onClickBtn = useCallback(()=>{
+    
+  }, []);
+
+  return(
+    <div>
+      <input type="number" placeholder="세로" value={row} onChange={onChangeRow} />
+      <input type="number" placeholder="가로" value={cell} onChange={onChangeCell} />
+      <input type="number" placeholder="지뢰" value={mine} onChange={onChangeMine} />
+      <button onClick={onClickBtn}>시작</button>
+    </div>
+  );
+
+};
+
+export default Form;
---
+++ 8.mine_search/client.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDom from 'react-dom';
+// 리액트와 리액트 돔을 불러옴
+
+import MineSearch from './MineSearch';
+
+
+ReactDom.render(<MineSearch />, document.querySelector('#root'));

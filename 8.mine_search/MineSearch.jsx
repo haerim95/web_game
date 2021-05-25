6c30188c5759ecd4de4672d7bcbc6ab302dec5d3
haerim95 <tjffldi123@daum.net>
2021-05-26 01:03:54 +0900

    지뢰찾기 작업중

--- 8.mine_search/MineSearch.jsx
+++ 8.mine_search/MineSearch.jsx
@@ -8,13 +8,14 @@ export const CODE = {
   QUESTION : -2,
   FLAG: -3,
   QUESTION_MINE: -4,
-  FLAG_MINE: -4,
+  FLAG_MINE: -5,
   CLICKED_MINE: -6,
   OPENED: 0, // 0 이상이면 다 opened
 }
 
 export const TableContext = createContext({
   tableData: [],
+  halted: true,
   dispatch: () => {},
 
 });
@@ -23,6 +24,7 @@ const initialState = {
   tableData : [],
   timer: 0,
   result: '',
+  halted: true,
 };
 
 const plantMine = (row, cell, mine) => { //지뢰 심기
@@ -60,13 +62,127 @@ const plantMine = (row, cell, mine) => { //지뢰 심기
 };
 
 export const START_GAME = 'START_GAME';
+export const OPEN_CELL = 'OPEN_CELL';
+export const CLICK_MINE = 'CLICK_MINE';
+export const FLAG_CELL = 'FLAG_CELL';
+export const QUESTION_CELL = 'QUESTION_CELL';
+export const NORMALIZE_CELL = 'NORMALIZE_CELL';
 
 const reducer = (state, action) => {
   switch(action.type){
     case START_GAME: {
       return {
         ...state,
-        tableData: plantMine(action.row, action.cell, action.mine)
+        tableData: plantMine(action.row, action.cell, action.mine),
+        halted: false,
+      }
+    };
+    case OPEN_CELL : {
+      // 불변성 유지를 위해 ...으로 저장
+      const tableData = [...state.tableData];
+      tableData.forEach((row, i) => {
+        tableData[i] = [...state.tableData[i]];
+      });
+      const checkArround = (row, cell) => { //내 기준으로 칸을 검사하는 함수
+        if([CODE.OPENED, CODE.FLAG_MINE, CODE_FLAG, CODE.QUESTION_MINE, CODE.QUESTION].includes(tableData[row][cell])){
+          
+        }
+        let around = [];
+        if(tableData[row -1]){ //윗줄이 있을때
+          around = around.concat( // 윗줄 세칸 검사
+            tableData[row -1][cell -1], 
+            tableData[row -1][cell],
+            tableData[row -1][cell + 1],
+            );
+        };
+        around = around.concat(
+          tableData[row -1][cell -1], 
+          tableData[row -1][cell + 1],
+        );
+        if(tableData[row + 1]){ //아랫줄 검사
+          around = around.concat(
+            tableData[row +1][cell -1], 
+            tableData[row +1][cell],
+            tableData[row +1][cell + 1],
+          );
+        };
+        const count = around.filter((v) => [CODE.MINE, CODE.QUESTION_MINE, CODE.FLAG_MINE].includes(v)).length;
+        console.log(around, count)
+        tableData[row][cell] = count;
+        if(count === 0){ // 주변 칸들이 없을 때
+          const near = [];
+          if(row -1 > -1){ // 제일 윗칸을 클릭
+            near.push([row - 1, cell - 1]);
+            near.push([row - 1, cell]);
+            near.push([row - 1, cell + 1]);
+          }
+          near.push([row, cell -1]);
+          near.push([row, cell +1]);
+          if(row +1 > tableData.length){ //제일 아랫칸 클릭
+            near.push([row + 1, cell - 1]);
+            near.push([row + 1, cell]);
+            near.push([row + 1, cell + 1]);
+          }
+          near.filter(v => !!v).forEach((n)=>{
+            checkArround(n[0], n[1]);
+          });
+        }else{
+          
+        }
+      };
+      checkArround(action.row, action.cell);
+      return{
+        ...state,
+        tableData,
+      };
+    };
+    case CLICK_MINE:{
+      const tableData = [...state.tableData];
+      tableData[action.row] = [...state.tableData[action.row]];
+      tableData[action.row][action.cell] = CODE.CLICKED_MINE; //클릭한 셀이 opned로 변경
+      return{
+        ...state,
+        tableData,
+        halted: true,
+      }
+    };
+    case FLAG_CELL:{
+      const tableData = [...state.tableData];
+      tableData[action.row] = [...state.tableData[action.row]];
+      if(tableData[action.row][action.cell] === CODE.MINE){
+        tableData[action.row][action.cell] = CODE.FLAG_MINE;
+      }else{
+        tableData[action.row][action.cell] = CODE.FLAG;
+      }
+      return{
+        ...state,
+        tableData,
+      }
+    };
+    case QUESTION_CELL: {
+      const tableData = [...state.tableData];
+      tableData[action.row] = [...state.tableData[action.row]];
+      if(tableData[action.row][action.cell] === CODE.FLAG_MINE){ //지뢰가 있는 깃발일때
+        tableData[action.row][action.cell] = CODE.QUESTION_MINE;
+      }else{
+        tableData[action.row][action.cell] = CODE.QUESTION; //지뢰가 없는 깃발일때
+      }
+      return{
+        ...state,
+        tableData,
+      }
+    };
+    case NORMALIZE_CELL: {
+      const tableData = [...state.tableData];
+      tableData[action.row] = [...state.tableData[action.row]];
+      if(tableData[action.row][action.cell] === CODE.QUESTION_MINE){ //물음표 칸에서 지뢰가 있을 때
+        tableData[action.row][action.cell] = CODE.MINE;
+      }else{
+        tableData[action.row][action.cell] = CODE.NORMAL; //물음표 칸에서 지뢰가 없을 때
+      }
+      return{
+        ...state,
+        tableData,
       }
     };
     default:
@@ -76,16 +192,17 @@ const reducer = (state, action) => {
 
 const MineSearch = () => {
   const [state, dispatch] = useReducer(reducer, initialState);
+  const { tableData, halted, timer, result } = state;
 
-  const value = useMemo(()=> ({ tableData: state.tableData, dispatch }), [state.tableData]);
+  const value = useMemo(()=> ({ tableData, halted, dispatch }), [tableData, halted]);
 
   return (
     //createContext를 사용하기 위해선 provider로 한 번 감싸야한다.
     <TableContext.Provider value={value}>
       <Form />
-      <div>{state.timer}</div>
+      <div>{timer}</div>
       <Table />
-      <div>{state.result}</div>
+      <div>{result}</div>
     </TableContext.Provider>
   );
 };
--- 8.mine_search/Td.jsx
+++ 8.mine_search/Td.jsx
@@ -1,4 +1,4 @@
-import React, {useContext} from 'react';
+import React, {useCallback, useContext} from 'react';
 import { CLICK_MINE, CODE, FLAG_CELL, NORMALIZE_CELL, OPEN_CELL, QUESTION_CELL, TableContext } from './MineSearch';
 
 const getTdStyle = (code) => {
@@ -9,10 +9,21 @@ const getTdStyle = (code) => {
         background: '#444444',
         color: '#ffffff',
       };
+    case CODE.CLICKED_MINE:
     case CODE.OPENED:
       return{
         background: '#ffffff',
         color: '#444444',
+      };
+    case CODE.QUESTION_MINE:
+    case CODE.QUESTION:
+      return{
+        background: 'yellow',
+      };
+    case CODE.FLAG_MINE:
+    case CODE.FLAG:
+      return{
+        background: 'red',
       };
     default:
       return{
@@ -28,16 +39,75 @@ const getTdText = (code) => {
       return '';
     case CODE.MINE:
       return 'X';
+    case CODE.CLICKED_MINE:
+      return '펑!';
+    case CODE.FLAG_MINE:
+    case CODE.FLAG:
+      return '!';
+    case CODE.QUESTION_MINE:
+    case CODE.QUESTION:
+      return '?';
     default:
-      return '';
+      return code || '';
   }
 };
 
 const Td = ( {rowIndex, cellIndex} ) => {
-  const {tableData} = useContext(TableContext);
+  const {tableData, dispatch, halted} = useContext(TableContext);
+
+  const onClickTd = useCallback( () => {
+    if(halted){
+      return;
+    }
+    switch(tableData[rowIndex][cellIndex]){
+      //클릭이 안되게 하는 부분
+      case CODE.OPENED:
+      case CODE.FLAG_MINE:
+      case CODE.FLAG:
+      case CODE.QUESTION_MINE:
+      case CODE.QUESTION:
+        return; //끝
+      case CODE.NORNAL: //보통인 칸, 클릭하면 열림
+        dispatch({type: OPEN_CELL, row: rowIndex, cell: cellIndex});
+        return;
+      case CODE.MINE: //지뢰칸 펑 ! 터진다
+        dispatch({ type: CLICK_MINE, row: rowIndex, cell: cellIndex });
+        return;
+      default:
+        return;
+    }
+    
+  }, [tableData[rowIndex][cellIndex], halted]);
+
+  const onRightClickTd = useCallback( (e) => {
+    e.preventDefault(); // 오른쪽 마우스 클릭할시 나오는 메뉴 안나오게끔하기 위해
+    if(halted){
+      return;
+    }
+    switch(tableData[rowIndex][cellIndex]){
+      case CODE.NORMAL: //일반칸은 깃발칸
+      case CODE.MINE:
+        dispatch({ type: FLAG_CELL, row: rowIndex, cell: cellIndex });
+        return;
+      case CODE.FLAG_MINE: //깃발칸은 물음표 컨
+      case CODE.FLAG:
+        dispatch({ type: QUESTION_CELL, row: rowIndex, cell: cellIndex });
+        return;
+        case CODE.QUESTION_MINE: // 물음표 칸은 일반 칸
+      case CODE.QUESTION: 
+        dispatch({ type: NORMALIZE_CELL, row: rowIndex, cell: cellIndex });
+        return;
+      default:
+        return;
+      // 서로서로 순환구조임 일반->깃발->물음표->일반
+    }
+  }, [tableData[rowIndex][cellIndex], halted])
 
   return(
-    <td style={getTdStyle(tableData[rowIndex][cellIndex])}
+    <td 
+      style={getTdStyle(tableData[rowIndex][cellIndex])}
+      onClick={onClickTd}
+      onContextMenu={onRightClickTd}
     >{getTdText(tableData[rowIndex][cellIndex])}</td>
   )
 };
